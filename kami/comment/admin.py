8c4d2ddd7db175c7b43508b7b859310ca539e452
Kungreye <kungreye@sina.com>
2020-04-15 01:07:30 +0800

    config/admin.py

--- kami/comment/admin.py
+++ kami/comment/admin.py
@@ -6,3 +6,6 @@ from .models import Comment
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('target', 'nickname', 'content', 'website', 'created_time')
+
+    def save_model(self, request, obj, form, change):
+        return super(CommentAdmin, self).save_model(request, obj, form, change)
