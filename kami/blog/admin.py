52b9fd21efce487a5d0f765115667c57ef0a2b49
Kungreye <kungreye@sina.com>
2020-04-15 01:45:15 +0800

    add custom filter CategoryOwnerFilter too PostAdmin

--- kami/blog/admin.py
+++ kami/blog/admin.py
@@ -29,14 +29,29 @@ class TagAdmin(admin.ModelAdmin):
         return super(TagAdmin, self).save_model(request, obj, form, change)
 
 
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    """自定义过滤器，只展示当前用户定义的分类"""
+    title = '分类过滤器'
+    parameter_name = 'owner_category'
+
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list('id', 'name')
+
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=self.value())
+        return queryset
+
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = [
         'title', 'category', 'status',
-        'created_time', 'operator',
+        'created_time', 'operator', 'owner'
     ]
     list_display_links = []
-    list_filter = ['category', ]
+    list_filter = [CategoryOwnerFilter, ]
     search_fields = ['title', 'category__name']
 
     actions_on_top = True
